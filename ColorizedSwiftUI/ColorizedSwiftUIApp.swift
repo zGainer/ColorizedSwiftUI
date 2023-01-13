74567738638cf9ed50bb7efc4222b258e1530704
Eugene <zgainer@gmail.com>
2023-01-13 10:21:02 +0300

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Eugene on 13.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
